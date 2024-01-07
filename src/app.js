558ac3e9db3cfd514afb7ec2076b7fef870b7083
jake <gerion@gmail.com>
2024-01-07 18:20:58 +0900

    preset-react 적용

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
+/* @jsx createElement */
 import { createDOM, createElement, render } from './react'
 
-const vdom = createElement('p', {},
-    createElement('h1', {}, "React 만들기"),
-    createElement('ul', {},
-        createElement('li', {style: "color:red"}, "첫번째 아이템"),
-        createElement('li', {style: "color:blue"}, "두번째 아이템"),
-        createElement('li', {style: "color:green"}, "세번째 아이템"),
-    )
-);
+// const vdom = createElement('p', {},
+//     createElement('h1', {}, "React 만들기"),
+//     createElement('ul', {},
+//         createElement('li', {style: "color:red"}, "첫번째 아이템"),
+//         createElement('li', {style: "color:blue"}, "두번째 아이템"),
+//         createElement('li', {style: "color:green"}, "세번째 아이템"),
+//     )
+// );
 
-console.log(vdom);
+const vdom = <p>
+    <h1>React 만들기</h1>
+    <ul>
+        <li style="color:red">첫번째 아이템</li>
+        <li style="color:blue">두번째 아이템</li>
+        <li style="color:green">세번째 아이템</li>
+    </ul>
+</p>
+
+// console.log(vdom);
 
 render(vdom, document.querySelector('#root'));
